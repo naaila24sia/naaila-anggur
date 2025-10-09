142fba9f73c2f392e32926e2b699fa23cd86fd83
naaila24sia <naaila24si@mahasiswa.pcr.ac.id>
2025-10-09 10:27:01 +0700

    membuat assets-admin

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\QuestionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
@@ -18,7 +19,7 @@ Route::get('/mahasiswa', function () {
 })->name('mahasiswa.show');
 
 Route::get('/nama/{param1}', function ($param1 = '') {
-    return 'Nama saya: '.$param1;
+    return 'Nama saya: ' . $param1;
 });
 
 Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
@@ -27,4 +28,11 @@ Route::get('/about', function () {
     return view('halaman-about');
 })->name('route-about');
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])
+    ->name('home');
+
+Route::post('question/store', [QuestionController::class, 'store'])
+    ->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
