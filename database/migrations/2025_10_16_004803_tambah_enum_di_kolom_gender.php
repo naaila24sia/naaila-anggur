7bbd357c9b719bf896666e901c782a690a53e559
naaila24sia <naaila24si@mahasiswa.pcr.ac.id>
2025-10-16 11:17:21 +0700

    Pertemuan 6

---
+++ database/migrations/2025_10_16_004803_tambah_enum_di_kolom_gender.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pelanggan', function (Blueprint $table) {
+            // Mengubah tipe kolom menjadi ENUM baru dan membuatnya nullable
+            $table->enum('gender', ['Pria', 'Wanita', 'Non-Binary'])->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pelanggan', function (Blueprint $table) {
+            // Contoh kembali ke ENUM lama (jika kita tahu nilainya)
+            $table->enum('gender', ['Male', 'Female', 'Other'])->nullable()->change();
+        });
+    }
+};
