8c6af18b4ee2c7507064b98fedbafabc59ca7a60
naaila24sia <naaila24si@mahasiswa.pcr.ac.id>
2025-11-27 14:52:57 +0700

    multiple file upload

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -61,17 +61,15 @@
                                 <div class="col-md-2">
                                     <select name="gender" class="form-select" onchange="this.form.submit()">
                                         <option value="">All</option>
-                                        <option value="Male" {{ request('gender') == 'Male' ? 'selected' : '' }}>Male
-                                        </option>
-                                        <option value="Female" {{ request('gender') == 'Female' ? 'selected' : '' }}>Female
-                                        </option>
+                                        <option value="Male" {{ request('gender') == 'Male' ? 'selected' : '' }}>Male</option>
+                                        <option value="Female" {{ request('gender') == 'Female' ? 'selected' : '' }}>Female</option>
                                     </select>
                                 </div>
                                 <div class="col-md-3">
                                     <div class="input-group">
-                                        <input type="text" name="search" class="form-control" id="exampleInputIconRight"
-                                            value="{{ request('search') }}" placeholder="Search" aria-label="Search">
-                                        <button type="submit" class="input-group-text" id="basic-addon2">
+                                        <input type="text" name="search" class="form-control" value="{{ request('search') }}"
+                                            placeholder="Search">
+                                        <button type="submit" class="input-group-text">
                                             <svg class="icon icon-xxs" fill="currentColor" viewBox="0 0 20 20"
                                                 xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd"
@@ -80,13 +78,14 @@
                                             </svg>
                                         </button>
                                         @if (request('search'))
-                                                <a href="{{ request()->fullUrlWithQuery(['search' => null]) }}"
-                                                    class="btn btn-outline-secondary ml-3" id="clear-search"> Clear</a>
-                                            @endif
+                                            <a href="{{ request()->fullUrlWithQuery(['search' => null]) }}"
+                                                class="btn btn-outline-secondary ml-3" id="clear-search"> Clear</a>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
                         </form>
+
                         <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
                             <thead class="thead-light">
                                 <tr>
@@ -109,41 +108,30 @@
                                         <td>{{ $item->email }}</td>
                                         <td>{{ $item->phone }}</td>
                                         <td>
+                                            {{-- TOMBOL DETAIL (WAJIB UNTUK MULTIPLE UPLOAD FILE) --}}
+                                            <a href="{{ route('pelanggan.show', $item->pelanggan_id) }}"
+                                                class="btn btn-primary btn-sm">Detail</a>
+
+                                            {{-- TOMBOL EDIT --}}
                                             <a href="{{ route('pelanggan.edit', $item->pelanggan_id) }}"
-                                                class="btn btn-info btn-sm">
-                                                <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
-                                                    stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
-                                                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10">
-                                                    </path>
-                                                </svg>
-                                                Edit
-                                            </a>
+                                                class="btn btn-info btn-sm">Edit</a>
+
+                                            {{-- TOMBOL HAPUS --}}
                                             <form action="{{ route('pelanggan.destroy', $item->pelanggan_id) }}"
-                                                method="POST" style="display:inline">
+                                                method="POST" style="display:inline"
+                                                onsubmit="return confirm('Yakin ingin menghapus data ini?');">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-sm">
-                                                    <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
-                                                        stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
-                                                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0">
-                                                        </path>
-                                                    </svg>
-                                                    Hapus
-                                                </button>
+                                                <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
                                             </form>
                                         </td>
                                     </tr>
                                 @endforeach
-
                             </tbody>
                         </table>
+
                         <div class="mt-3">
                             {{ $dataPelanggan->links('pagination::bootstrap-5') }}
-                            {{-- {{ $dataPelanggan->links('pagination::simple-bootstrap-5') }} --}}
                         </div>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,15 @@ Route::post('question/store', [QuestionController::class, 'store'])
 Route::get('dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
+    
 Route::resource('pelanggan', PelangganController::class);
 
+Route::post('/pelanggan/upload-files', [PelangganController::class, 'uploadFiles'])
+    ->name('pelanggan.upload-files');
+
+Route::delete('/pelanggan/file/{fileId}', [PelangganController::class, 'deleteFile'])
+    ->name('pelanggan.delete-file');
+
 Route::resource('user', UserController::class);
+
+
